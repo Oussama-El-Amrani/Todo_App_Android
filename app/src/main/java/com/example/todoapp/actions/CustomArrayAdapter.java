aa2e56cc2aa6e96ddb4e7da3c298792f463a49d4
Oussama-El-Amrani <96589855+Oussama-El-Amrani@users.noreply.github.com>
2023-03-18 00:06:38 +0100

    Fix issue with deleting last item in CustomArrayAdapter

--- app/src/main/java/com/example/todoapp/actions/CustomArrayAdapter.java
+++ app/src/main/java/com/example/todoapp/actions/CustomArrayAdapter.java
@@ -35,49 +35,50 @@ public class CustomArrayAdapter extends ArrayAdapter<Todo> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View v, @NonNull ViewGroup parent) {
-        View layout = v;
-        if(layout == null) {
-            layout = LayoutInflater.from(mContext).inflate(resource,parent,false);
+        View view = v;
+        if (view == null) {
+            view = LayoutInflater.from(mContext).inflate(resource, parent, false);
 
-            CheckBox isCompletedCheckBox = layout.findViewById(R.id.todoCheckBo);
-            Button deleteButton = layout.findViewById(R.id.detete_button);
+            CheckBox isCompletedCheckBox = view.findViewById(R.id.todoCheckBo);
+            Button deleteButton = view.findViewById(R.id.detete_button);
 
-            Todo todo = getItem(position);
-
-            isCompletedCheckBox.setText(todo.getTitle());
-            isCompletedCheckBox.setChecked(todo.isCompleted());
-
-            layout.setBackgroundColor(
-                    mContext.getResources().getColor(R.color.information)
-            );
-
-            deleteButton.setTag(todo);
-            deleteButton.setOnClickListener(deleteTodo);
-
-            isCompletedCheckBox.setTag(todo);
+            isCompletedCheckBox.setTag(isCompletedCheckBox);
             isCompletedCheckBox.setOnClickListener(completedTodo);
-        } else {
-            Todo todo = (Todo) layout.findViewById(R.id.detete_button).getTag();
-            layout.findViewById(R.id.todoCheckBo).setTag(todo);
-            layout.findViewById(R.id.detete_button).setTag(todo);
+
+            deleteButton.setTag(deleteButton);
+            deleteButton.setOnClickListener(deleteTodo);
         }
 
-        return layout;
+        Todo todo = getItem(position);
+
+        CheckBox isCompletedCheckBox = view.findViewById(R.id.todoCheckBo);
+        Button deleteButton = view.findViewById(R.id.detete_button);
+
+        isCompletedCheckBox.setText(todo.getTitle());
+        isCompletedCheckBox.setChecked(todo.isCompleted());
+
+        view.setBackgroundColor(mContext.getResources().getColor(R.color.information));
+
+        isCompletedCheckBox.setTag(todo);
+        deleteButton.setTag(todo);
+
+        return view;
     }
 
     private View.OnClickListener completedTodo = v -> {
         CheckBox checkBox = (CheckBox) v;
-        Todo todo = (Todo) v.getTag();
-        todo.setCompleted(checkBox.isChecked());
-        mServices.updateTodo(todo);
+        Todo todo_ = (Todo) v.getTag();
+        todo_.setCompleted(checkBox.isChecked());
+        mServices.updateTodo(todo_);
         notifyDataSetChanged(); // notify the adapter that the data has changed
     };
 
     private View.OnClickListener deleteTodo = v -> {
         Button button = (Button) v;
-        Todo todo = (Todo) v.getTag();
-        mServices.deleteTodo(todo);
-        remove(todo); // remove the todo from the adapter's list
+        Todo todo_ = (Todo) v.getTag();
+        System.out.println(todo_);
+        mServices.deleteTodo(todo_);
+        remove(todo_); // remove the todo from the adapter's list
         notifyDataSetChanged(); // notify the adapter that the data has changed
     };
 }
